0f187d098365afd804564675c0310e97111c2fb0
TheBlindGhost <mateusz_pietrusa@onet.pl>
2024-12-12 19:18:01 +0100

    Setters

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -7,7 +7,31 @@ namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public required string Description {
+        get { return description; }
+        init
+        {
+                string a = value.Trim();
+                value = a;
+                if (value.Length < 3)
+                {
+                    for (int i = value.Length; i <= 2; i++)
+                    {
+                        value += "#";
+                    }
+
+                }
+                else if (value.Length > 15)
+                {
+                    value = value.Substring(0, 15);
+                    value = value.Trim();
+                }
+                string temp = value.Substring(0, 1);
+                description = temp.ToUpper() + value.Substring(1, value.Length - 1);
+
+        }
+        }
     public uint Size { get; set; } = 3;
 
     public string Info
--- Simulator/Creatures.cs
+++ Simulator/Creatures.cs
@@ -1,10 +1,62 @@
 ﻿
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
 namespace Simulator;
 
 public class Creatures
 {
-    public string? Name { get; set; }
-    public int Level { get; set; }
+
+    private string name = "Unknown";
+    private int level = 1;
+    public string? Name
+    {
+        get { return name; }
+        init
+        {
+
+                value = value.Trim();
+                if (value.Length < 3)
+                {
+                    for (int i = value.Length; i <= 2; i++)
+                    {
+                        value += "#";
+                    }
+
+                }
+                else if (value.Length > 25)
+                {
+                    value = value.Substring(0, 25);
+                    value = value.Trim();
+                }
+                string temp = value.Substring(0, 1);
+                name = temp.ToUpper() + value.Substring(1, value.Length - 1);
+
+        }
+    }
+
+
+    public int Level
+    {
+        get { return level; }
+        init
+        {
+                if (value < 1) { level = 1; }
+                else if (value > 10) { level = 10; }
+                else { level = value; }
+
+        }
+
+
+    }
+
+
+    public void Upgrade()
+    {
+        if (level < 10) { level += 1; }
+
+    }
+
 
     public Creatures(string name, int level = 1)
     {
@@ -13,7 +65,7 @@ public class Creatures
 
     }
 
-    public Creatures (){}
+    public Creatures() { }
 
     public string Info
     {
