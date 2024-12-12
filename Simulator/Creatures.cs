b7c86482ec6d5c0245e561b8e098753e783de7f5
TheBlindGhost <mateusz_pietrusa@onet.pl>
2024-12-12 17:00:48 +0100

    Add project files.

---
+++ Simulator/Creatures.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Simulator;
+
+public class Creatures
+{
+    public string? Name { get; set; }
+    public int Level { get; set; }
+
+    public Creatures(string name, int level = 1)
+    {
+        this.Name = name;
+        this.Level = level;
+
+    }
+
+
+    public Creatures (){}
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+
+
+}
+
---
+++ Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Simulator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Simulator!\n");
+
+    }
+}
+
+
